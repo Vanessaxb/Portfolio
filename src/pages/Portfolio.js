b34fed58b15fe1afaf4c16af2eb5e0ee319c72c3
Vanessa Beattie <vanessabeattie@Vanessas-MacBook-Pro.local>
2023-08-21 16:49:58 -0500

    Portfolio updated2

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -8,7 +8,7 @@ export default function Portfolio() {
       <h1 className="flex justify-center mt-10 md:mt-36 font-black text-gray-500 text-3xl md:text-6xl font-raleway text-center ">
         PROJECTS
       </h1>
-      <div className=" items-center p-20 mt-6 md:mt-20 ">
+      <div className=" items-center p-20 mt-4 md:mt-20 ">
         <div className="flex flex-col md:flex-row justify-center items-center">
           <div className="rounded-3xl p-1 shadow bg-gray-200/80 md:mr-4 mb-4 md:mb-0">
             <ReactPlayer
@@ -18,17 +18,17 @@ export default function Portfolio() {
               height="420px"
             />
           </div>
-          <div className="md:w-1/2 mt-2 md:mt-0">
+          <div className="md:w-1/2 mt-4 md:-mt-16 mt">
             <h1 className=" text-2xl md:text-4xl text-green-500 font-raleway mb-3 md:mb-5 text-center ">
               Memory Game
             </h1>
-            <p className="font-raleway text-base md:text-lg text-center mb-4">
+            <p className="font-raleway text-base md:text-lg text-center mb-1 md:mb-1">
               This kid's memory game features: a Win state, <br /> a Lose state,
               Timer, Multiple Rounds, <br /> a Reset-Button and a Click-Counter.{" "}
               <br />
               It was built with CSS with FlexBox and JavaScript.
             </p>
-            <div className="flex justify-center p-8 mr:8 md:mr-16 ">
+            <div className="flex justify-center p-4 mr:8 md:mr-16 ">
               <a
                 href="https://github.com/Vanessaxb/Memory-Game.git"
                 target="_blank"
@@ -47,25 +47,25 @@ export default function Portfolio() {
             </div>
           </div>
         </div>
-        <div className="flex flex-col md:flex-row justify-center items-center mt-14 md:mt-28 ml-4 md:ml-28">
+        <div className="flex flex-col md:flex-row justify-center items-center mt-20 md:mt-28 ml-4 md:ml-28">
           <div className="md:w-1/2">
             <img
-              className="h-[450px] md:h-[700] w-full md:w-[490px] rounded-3xl p-1 shadow bg-gray-200/80"
+              className="h-[350px] md:h-[700] w-[245px] md:w-[490px] rounded-3xl p-1 shadow bg-gray-200/80"
               src={require("../images/StockApp.png")}
               alt="Stock App"
             />
           </div>
-          <div className="md:w-1/2 md:mr-52 mt-2 md:mt-0">
+          <div className="md:w-1/2 md:mr-52 mt-2 md:mt-0 ">
             <h1 className=" text-2xl md:text-4xl text-green-500 font-raleway mb-3 md:mb-5 text-center md:text-center">
               Stocks App
             </h1>
-            <p className="font-raleway text-base md:text-lg text-center mb-2">
+            <p className="font-raleway text-base md:text-lg text-center mb-1 md:mb-1">
               If you'd like to check live rates for your <br />
               favorite stocks, look no further. <br /> This was school
               assignment was lots of fun. <br/> It was built with
               CSS with FlexBox and JavaScript.
             </p>
-            <div className="flex justify-center p-8">
+            <div className="flex justify-center p-2 md:p-4">
               <a
                 href="https://github.com/Vanessaxb/stocks-app.git"
                 target="_blank"
